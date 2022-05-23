9863fce844d7cc6cd1e39acf0aa4ea3600de001c
utkarsh <90375318+UtkarshPatel9934@users.noreply.github.com>
2022-05-23 19:16:50 -0400

    Added a code to style the cards if they are on even or odd index.

--- U_Patel_DoraemonCharacter/src/app/content-card/content-card.component.ts
+++ U_Patel_DoraemonCharacter/src/app/content-card/content-card.component.ts
@@ -3,11 +3,13 @@ import {Content} from '../models/content'
 @Component({
   selector: 'app-content-card',
   templateUrl: './content-card.component.html',
-  styleUrls: ['./content-card.component.css']
+  styleUrls: ['./content-card.component.scss']
 })
 export class ContentCardComponent implements OnInit {
 
   @Input() charactersContent?: Content;
+
+
 
 
   constructor() { }

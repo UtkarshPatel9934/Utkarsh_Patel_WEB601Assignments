55346de7b4f674a01b459b99ff22120163919bed
utkarsh <90375318+UtkarshPatel9934@users.noreply.github.com>
2022-06-06 16:31:04 -0400

    Added the mouseover event in appHighlightImportantData

--- U_Patel_DoraemonCharacter/src/app/directive/highlight-important-data.directive.ts
+++ U_Patel_DoraemonCharacter/src/app/directive/highlight-important-data.directive.ts
@@ -1,10 +1,32 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef,HostBinding, HostListener, Input } from '@angular/core';
 
 @Directive({
   selector: '[appHighlightImportantData]'
 })
 export class HighlightImportantDataDirective {
 
-  constructor() { }
+  // @Input() colour?: string;
+  private checkIsHighlighted: boolean = false;
+  // private grabTypeEle;
+
+  @HostBinding('style.border')
+  get applyBorderColor()
+  {
+    // return this.checkIsHighlighted ? "2px solid red" : "2px solid green";
+    return this.checkIsHighlighted ? "2px solid gold" : "none";
+  }
+
+
+  constructor(private grabElm: ElementRef) {
+    this.grabElm.nativeElement.style.border;
+  }
+
+
+
+  @HostListener('mouseover') onEnterInTypeTag() {
+    this.checkIsHighlighted = !this.checkIsHighlighted;
+  }
+
+
 
 }

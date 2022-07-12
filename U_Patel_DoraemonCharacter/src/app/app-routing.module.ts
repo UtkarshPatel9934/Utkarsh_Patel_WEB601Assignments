6cb1befca7cfe3c0279688f0f520963df364b6ae
utkarsh <90375318+UtkarshPatel9934@users.noreply.github.com>
2022-07-12 19:32:35 -0400

    Added route for /updateContent

--- U_Patel_DoraemonCharacter/src/app/app-routing.module.ts
+++ U_Patel_DoraemonCharacter/src/app/app-routing.module.ts
@@ -31,6 +31,10 @@ const routes: Routes = [
     component: ChangeContentComponent
   },
   {
+    path: "updateContent/:id",
+    component: ChangeContentComponent
+  },
+  {
     path: "**",
     component: InvalidRouteComponentComponent
   }

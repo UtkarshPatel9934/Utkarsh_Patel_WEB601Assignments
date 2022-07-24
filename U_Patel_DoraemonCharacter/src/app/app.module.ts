b6e61f9e2f1c7990025fbb642aaf145f62b20d76
utkarsh <90375318+UtkarshPatel9934@users.noreply.github.com>
2022-07-23 23:17:34 -0400

    Update the content card layouts and design

--- U_Patel_DoraemonCharacter/src/app/app.module.ts
+++ U_Patel_DoraemonCharacter/src/app/app.module.ts
@@ -25,8 +25,8 @@ import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { MatButtonModule } from '@angular/material/button';
- import { MatInputModule } from '@angular/material/input';
-
+import { MatInputModule } from '@angular/material/input';
+import {MatCardModule} from '@angular/material/card';
 
 
 @NgModule({
@@ -47,6 +47,7 @@ import { MatButtonModule } from '@angular/material/button';
     BrowserModule,
     MatButtonModule,
     MatInputModule,
+    MatCardModule,
     FormsModule,
     AppRoutingModule,
     HttpClientModule,
